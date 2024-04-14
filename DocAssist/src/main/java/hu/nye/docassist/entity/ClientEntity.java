6eeef313be79a7b358d170fdf48d4c23b78f8205
assassino1792 <103066469+assassino1792@users.noreply.github.com>
2024-04-14 11:01:17 +0200

    teszt db

--- DocAssist/src/main/java/hu/nye/docassist/entity/ClientEntity.java
+++ DocAssist/src/main/java/hu/nye/docassist/entity/ClientEntity.java
@@ -1,4 +1,109 @@
 package hu.nye.docassist.entity;
 
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
 public class ClientEntity {
+    private static final Logger logger = LoggerFactory.getLogger(ClientEntity.class);
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty
+    private String firstName;
+    @NotEmpty
+    private String lastName;
+    @PositiveOrZero
+    private Integer age;
+    @NotEmpty
+    private String doctorsName;
+    @NotEmpty
+    private String disease;
+
+    @JsonFormat(pattern = "yyyy.MM.dd")
+    private LocalDate registrationDate;
+
+
+
+    // konstruktor
+    public ClientEntity(String firstName, String lastName, Integer age, String doctorsName, String disease) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.doctorsName = doctorsName;
+        this.disease = disease;
+        logger.info("ClientEntity created: {} {}", firstName, lastName, age, doctorsName, disease);
+    }
+
+    // Getterek és setterek
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getDoctorsName() {
+        return doctorsName;
+    }
+
+    public void setDoctorsName(String doctorsName) {
+        this.doctorsName = doctorsName;
+    }
+
+    public String getDisease() {
+        return disease;
+    }
+
+    public void setDisease(String disease) {
+        this.disease = disease;
+    }
 }
--- DocAssist/src/main/java/hu/nye/docassist/repository/ClientRepository.java
+++ DocAssist/src/main/java/hu/nye/docassist/repository/ClientRepository.java
@@ -1,4 +1,12 @@
 package hu.nye.docassist.repository;
 
-public class ClientRepository {
+import hu.nye.docassist.entity.ClientEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Repository
+public interface ClientRepository extends JpaRepository<ClientEntity, Long> {
+    Logger logger = LoggerFactory.getLogger(ClientRepository.class);
 }
